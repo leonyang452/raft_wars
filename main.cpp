1b15d83f366ba4911c0d8e9f125786e62929fe08
Leon Yang <leonyang451@gmail.com>
2025-08-25 01:37:17 +0100

    loaded a character onto the screen

--- main.cpp
+++ main.cpp
@@ -3,10 +3,30 @@
 #include <raylib.h>
 using namespace std;
 
+/**
+ * @brief Takes the relative file path of an image and loads it into the game window
+ * 
+ * 
+ * @param filePath The relative file path of where the image is stored -> Type: const char*
+ * 
+ * 
+ * @return texture of the image -> Type: Texture2D
+ */
+Texture2D loadImage(const char* filePath){
+    Image p1 = LoadImage(filePath);
+    Texture2D texture = LoadTextureFromImage(p1);
+    UnloadImage(p1);
+    return texture;
+}
+
 int main(){
+    int ballX = 400;
+    int ballY = 400;
     int GAME_WIDTH = 1500;
     int GAME_HEIGHT = 500;
+    Color green = {20, 160, 133, 255};
     InitWindow(GAME_WIDTH, GAME_HEIGHT, "Raft Wars");
+    Texture2D owletImage = loadImage("assets/Owlet_Monster.png");
 
     SetTargetFPS(60); // 60 times per second
 
@@ -20,6 +40,7 @@ int main(){
         // 3. Drawing
         BeginDrawing();
         ClearBackground(BLACK);
+        DrawTexture(owletImage, GAME_WIDTH/2 - owletImage.width/2, GAME_HEIGHT/2 - owletImage.height/2, WHITE);
         EndDrawing();
     }
     CloseWindow();
